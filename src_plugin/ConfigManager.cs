4d4f9726009a9d18c4b1b4114c6d8e16be7ac7da
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-04-19 01:29:41 +1000

    Bump UniverseLib

--- src_plugin/ConfigManager.cs
+++ src_plugin/ConfigManager.cs
@@ -24,7 +24,7 @@ namespace ConfigManager
         public const string GUID = "com.sinai.BepInExConfigManager";
         public const string NAME = "BepInExConfigManager";
         public const string AUTHOR = "Sinai";
-        public const string VERSION = "1.2.1";
+        public const string VERSION = "1.2.2";
 
         public static ConfigManager Instance { get; private set; }
 
